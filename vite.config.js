ee43dcbfc8bcdc832bca33d482f39d92fa9ff5c2
hymn3333 <h79429768@gmail.com>
2025-04-25 11:42:27 +0900

    🔧 GitHub Pages 경로 설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/lessonfit/',   // ✅ 이 줄 추가
   plugins: [react()],
 })
